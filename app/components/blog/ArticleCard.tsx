470dc9312b28f384b08aa120834e8a89578c40e4
matefilip <filip.mateusz.95@gmail.com>
2024-07-07 13:38:32 +0200

    refactor card component

--- app/components/blog/ArticleCard.tsx
+++ app/components/blog/ArticleCard.tsx
@@ -7,25 +7,24 @@ const Card: React.FC<ArticlePreviewProps> = ({ article }) => {
   };
 
   return (
-<div
-  className="relative bg-white shadow-lg rounded-lg overflow-hidden cursor-pointer transition transform hover:scale-105 border border-gray-300"
-  onClick={handleClick}
-  style={{ height: 'calc(100% - 2px)' }}
->
-  <div className="relative h-full">
-    <img src={article.mediaUrl} alt={article.title} className="w-full h-full object-cover" />
-    <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <h2 className="text-lg font-bold text-white text-center px-2" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75)' }}>
-        {article.title}
-      </h2>
+    <div
+      className="relative bg-white shadow-lg rounded-lg overflow-hidden cursor-pointer transition transform hover:scale-105 border border-gray-300"
+      onClick={handleClick}
+    >
+      <div className="relative">
+        <div className="h-64 overflow-hidden">
+          <img src={article.mediaUrl} alt={article.title} className="w-full h-full object-cover" />
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h2 className="text-lg font-bold text-white text-center px-2" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75)' }}>
+            {article.title}
+          </h2>
+        </div>
+        <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-sm px-4 py-2">
+          Dodano: {new Date(article.addedAt).toLocaleDateString()}
+        </div>
+      </div>
     </div>
-  </div>
-  <div className="p-4 relative">
-    <div className="absolute bottom-10 left-2 text-sm text-gray-500 bg-white bg-opacity-60 px-2 py-1 rounded">
-      {new Date(article.addedAt).toLocaleDateString()}
-    </div>
-  </div>
-</div>
   );
 };
 
--- app/components/blog/RecommendedArticles.tsx
+++ app/components/blog/RecommendedArticles.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import Card from './ArticleCard';
+
+interface RecommendedArticle {
+  key: string;
+  title: string;
+  mediaUrl: string;
+  addedAt: string;
+}
 
 interface RecommendedArticlesProps {
   recommended: RecommendedArticle[];
@@ -11,24 +19,9 @@ const RecommendedArticles: React.FC<RecommendedArticlesProps> = ({ recommended }
   return (
     <div className="fixed right-0 top-0 p-6 h-full w-1/6 bg-gray-900 shadow-lg overflow-y-auto">
       <h2 className="text-xl text-white mb-4 font-bold border-b border-gray-700 pb-2">Również polecane</h2>
-      {recommended.map((rec, index) => (
-        <div
-          className={`relative bg-white shadow-lg rounded-lg overflow-hidden cursor-pointer transition transform hover:scale-105 border border-gray-300 mb-6 ${index !== 0 ? 'border-t border-gray-300 pt-0' : ''}`}
-          onClick={() => router.push(`/blog/article/${rec.key}`)}
-        >
-          <div className="relative h-full">
-            <img src={rec.mediaUrl} alt={rec.title} className="w-full h-full object-cover" />
-            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-              <h2 className="text-lg font-bold text-white text-center px-2" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75)' }}>
-                {rec.title}
-              </h2>
-            </div>
-          </div>
-          <div className="p-4 relative">
-            <div className="absolute bottom-0 right-1 text-sm text-gray-500 bg-white bg-opacity-60 px-2 py-1 rounded">
-              {new Date(rec.addedAt).toLocaleDateString()}
-            </div>
-          </div>
+      {recommended.map((rec) => (
+        <div key={rec.key} className="mb-6">
+          <Card article={rec} />
         </div>
       ))}
     </div>
